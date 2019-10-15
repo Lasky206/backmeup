da1dc20e23ed67e1cd39493166f2540e94dab76a
Lasky206 <mistermisterio1000@gmail.com>
2019-10-14 21:36:02 -0700

    Added SkyLinux

--- Backup.sh
+++ Backup.sh
@@ -37,7 +37,7 @@ restore () {
   #fetch packages
   wget -O atom.deb https://atom.io/download/deb
   wget https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb
-  wget https://www.tel.red/linux.php?f=sky_2.1.7458-1ubuntu%2Bbionic_amd64.deb
+  wget -O skylinux.deb https://www.tel.red/linux.php?f=sky_2.1.7458-1ubuntu%2Bbionic_amd64.deb
   wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   wget https://discordapp.com/api/download?platform=linux&format=deb
 
