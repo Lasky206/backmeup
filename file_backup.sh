494935651c0fbddcd6b9760aae0102c61b31a533
Lasky206 <mistermisterio1000@gmail.com>
2019-11-07 16:53:09 -0700

    Started working on file_backup

--- file_backup.sh
+++ file_backup.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 
-echo "TEST TEST TEST"
-sleep 5
+
+clear
+
+
+TARBALL=('~/Documents' '~/Pictures' '~/.ssh' '~/.certs' '/etc/NetworkManager/system-connections')
+
+
+dpkg -s atom &> /dev/null
+if [ $? == 0 ]; then TARBALL+=('~/.atom'); fi
+snap list remmina
+if [ $? == 0 ]; then TARBALL+=('~/snap/remmina/current/.local/share/remmina'); fi
+
+
+
+echo "TARBalling the following directories: "
+echo
+
+
+for i in ${TARBALL[*]}; do
+  echo $i
+done
+
+echo
