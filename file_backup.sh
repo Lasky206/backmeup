147447f4f345fb04618338e92226a7902795f720
Lasky206 <mistermisterio1000@gmail.com>
2019-11-08 00:50:37 -0700

    Added potential fix

--- file_backup.sh
+++ file_backup.sh
@@ -7,7 +7,7 @@ clear
 TARBALL=('~/Documents' '~/Pictures' '~/.ssh' '~/.certs' '/etc/NetworkManager/system-connections')
 
 
-print_list () {
+print_list() {
   clear
   echo "TARBalling the following directories: "
   echo
@@ -17,6 +17,22 @@ print_list () {
   echo
 }
 
+#
+# ###### EXPERIMENTAL ##########
+# add_to_list() {
+#   # Add custom backup locations
+#   read -r -p "Would you like to add any custom locations? [y/N]: " opt
+#
+#
+#   if [[ "$opt" =~ ^[Yy]$ ]]; then
+#     read -r -p "Enter the path you'd like to add: " opt
+#     echo $opt
+#   else
+#     :
+#   fi
+# }
+# ##############################
+#
 
 # Determin if certain packages are needed for backups
 dpkg -s atom &> /dev/null
@@ -36,7 +52,7 @@ read -r -p "Would you like to add any custom locations? [y/N]: " opt
 
 
 if [[ "$opt" =~ ^[Yy]$ ]]; then
-  read -r -p "Enter the path you'd like to add: " opt2
+  read -r -p "Enter the path you'd like to add: " opt
   TARBALL+=('test')
   ./file_backup.sh
 else
