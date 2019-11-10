2bfb83a70cf6e74b5c6658ce205c81049fe35e89
Lasky206 <mistermisterio1000@gmail.com>
2019-11-10 02:28:15 -0700

    Updated Atom and Python install scripts

--- Archive/Atom.sh
+++ Archive/Atom.sh
@@ -1,10 +1,46 @@
 #!/bin/bash
 
-#Prompt for dependencies before continue
+NC='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
 
-wget -O ./tmp/atom.deb https://github.com/atom/atom/releases/download/v1.41.0/atom-amd64.deb
-sudo dpkg -i ./tmp/atom.deb
+#Prompt for dependencies before continue
+read -r -p "Did you make a backup? [y/N]: " opt
+if [[ $opt =~ ^[Yy]$ ]]; then
+  read -r -p "Path to backup file: " opt
+  tar -xzf $opt -C ~/
+  if [ $? = 0 ]; then
+    clear
+    banner
+    printf "The backup ${GREEN}completed sucessfully${NC}"
+    sleep 5
+  else
+    clear
+    banner
+    printf "The backup ${RED}FAILED!${NC}"
+    sleep 5
+  fi
+else
+  :
+fi
+
+wget -O ./staging/atom.deb https://github.com/atom/atom/releases/download/v1.41.0/atom-amd64.deb
+sudo dpkg -i ./staging/atom.deb
 sudo apt install -f
+
+# Check to see if Python is installed
+dpkg -s Python &> /dev/null
+if [ $? == 0 ]; then
+  :
+else
+  echo "Python does not appear to be installed on this system. Some configurations may fail without it!"
+  read -r -p "Would you like to install Python now? [Y/n]: " opt
+  if [[ $opt =~ ^[Nn]$ ]]; then
+    :
+  else
+    ./Python.sh
+  fi
+fi
 
 pip install pycodestyle
 
--- Archive/Python.sh
+++ Archive/Python.sh
@@ -2,4 +2,4 @@
 
 wget -O ./staging/python.tgz https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tgz
 tar -xzf ./staging/python.tgz -C ./staging/
-./tmp/Python-3.8.0/install-sh
+./staging/Python-3.8.0/install-sh
