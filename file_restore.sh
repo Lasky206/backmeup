51c3cf992133d85c8ac1cf6ecacbdebf40c3644e
Lasky206 <mistermisterio1000@gmail.com>
2019-11-11 02:58:45 -0700

    file restore script updated

--- file_restore.sh
+++ file_restore.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 
-echo TEST2 TEST2 TEST2
-sleep 10
+
+banner() {
+  echo
+  echo "###########################################"
+  echo "##                                       ##"
+  echo "##               Restore                 ##"
+  echo "##                                       ##"
+  echo "###########################################"
+  echo
+}
+
+
+banner
+
+read -r -p "Enter the path to your backup file: " opt
+tar -xzf ${opt}/BMU_backup.tgz -C ~/
+if [ $? = 0 ]; then
+  clear
+  banner
+  printf "The backup ${GREEN}completed sucessfully${NC}"
+  sleep 5
+else
+  clear
+  banner
+  printf "The backup ${RED}FAILED!${NC}"
+  sleep 5
+fi
