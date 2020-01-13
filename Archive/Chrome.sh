11825bc6ef735f3919f0c12692eb337ef4e8f8dd
Lasky206 <43075806+Lasky206@users.noreply.github.com>
2020-01-13 11:47:41 -0700

    Added sudo to installer

--- Archive/Chrome.sh
+++ Archive/Chrome.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 wget -O ./staging/chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i ./staging/chrome.deb
+sudo dpkg -i ./staging/chrome.deb
 sudo apt install -f
